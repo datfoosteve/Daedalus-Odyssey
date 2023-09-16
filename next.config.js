cf5d23a961ecfbb08ca0d40fff5af6d6cf3748af
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2023-09-15 22:41:11 -0500

    tryomg

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 import { withContentlayer } from "next-contentlayer"
 
-import "./env.mjs"
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  }
 }
 
 export default withContentlayer(nextConfig)
