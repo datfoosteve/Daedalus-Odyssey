dec8909e4894975f3cdb5cf0726db4112fa91361
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2023-09-20 23:43:17 -0500

    removing blog from filePathPattern

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -18,7 +18,7 @@ const computedFields = {
 
 export const Doc = defineDocumentType(() => ({
   name: 'Doc',
-  filePathPattern: `blog/**/*.mdx`,
+  filePathPattern: `/**/*.mdx`,
   contentType: 'mdx',
   fields: {
     title: {
