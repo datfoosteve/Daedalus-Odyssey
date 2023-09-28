06369fe214c62f29e4d3798c796ebc7d644703ad
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2023-09-28 05:31:49 -0500

    ima try this

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,18 @@
+
+// import { WithClerkMiddleware } from "@clerk/nextjs/server";
+// import type { NextRequest } from "next/server";
+// import type { NextResponse } from "next/server";
+
+// export default withClerkMiddleware((_req: NextRequest) => {
+//   NextResponse.next()
+
+// })
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ["/", "/projects", "/blog", "/timeline", "/resume", /^\/blog\/.*/],
