5fefe1c36d32ce1168f0dae898dfdbf2dec654bb
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2023-10-02 05:20:12 -0500

    trying to see

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,37 +1,39 @@
 // src/app/projects/page.tsx
 
+import React, { useState, useEffect, FC } from 'react';  // Import React and necessary hooks
 import axios from 'axios';
 import RepositoryCard from '../../components/RepositoryCard';
 import Repository from '../../types/Repository';  // Adjust the import path to match your project structure
-
-interface ProjectsPageProps {
-    repositories: Repository[];
-  }
 
 // Define an async function to fetch data
 async function fetchRepositories() {
   const response = await axios.get('https://api.github.com/user/repos', {
     headers: {
-        Authorization: `token ${process.env.GITHUB_PAT}`,
+      Authorization: `token ${process.env.GITHUB_PAT}`,
     },
   });
   return response.data;
 }
 
-// Use getServerSideProps to fetch data server-side
-export async function getServerSideProps() {
-  const repositories = await fetchRepositories();
-  return { props: { repositories } };
+interface ProjectsPageProps {
+  params: { [key: string]: string };  // Define a prop type for params
 }
 
-const ProjectsPage: React.FC<ProjectsPageProps> = ({ repositories }) => {
-    return (
-      <div>
-        {repositories.map((repo: Repository) => (
-          <RepositoryCard key={repo.id} {...repo} />
-        ))}
-      </div>
-    );
-  };
+const ProjectsPage: FC<ProjectsPageProps> = ({ params }) => {
+  // Assume that `params.username` contains the GitHub username whose repositories you want to fetch
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  
+  useEffect(() => {
+    fetchRepositories().then(setRepositories);
+  }, []);
+  
+  return (
+    <div>
+      {repositories.map((repo: Repository) => (
+        <RepositoryCard key={repo.id} {...repo} />
+      ))}
+    </div>
+  );
+};
 
 export default ProjectsPage;
