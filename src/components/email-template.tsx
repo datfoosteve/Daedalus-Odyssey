6a2ee6b009d59cd7143b8a64014d422993ad6b38
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2023-10-22 06:13:21 -0500

    try and see if this works

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -12,7 +12,8 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName, email, phone, subject, message
 }) => (
   <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px', maxWidth: '600px', margin: '0 auto', backgroundColor: '#f9f9f9', borderRadius: '8px' }}>
-    <h1 style={{ fontSize: '24px', marginBottom: '10px', color: '#333' }}>Thank You, {firstName}!</h1>
+ <img src="https://stephenputhenpurackal.com/public/images/icon.png" alt="Website Logo" style={{ display: 'block', margin: '0 auto', marginBottom: '20px' }} />
+  <h1 style={{ fontSize: '24px', marginBottom: '10px', color: '#333' }}>Thank You, {firstName}!</h1>
     <p style={{ fontSize: '16px', marginBottom: '20px', color: '#555' }}>
       I've received your message and appreciate you reaching out. I'll make sure to review your inquiry and get back to you as soon as possible. Looking forward to discussing how I can assist you with your web development needs!
     </p>
