9be32c27609eacd510ba9809be1d0a70777f89d8
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2023-10-22 08:12:31 -0500

    like the styling

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,102 +1,139 @@
 "use client";
 
 import Footer from "@/components/Footer";
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const ContactPage: React.FC = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    subject: '',
-    message: ''
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: "",
   });
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const response = await fetch('/api/send', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData)
+    const response = await fetch("/api/send", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
     });
     const data = await response.json();
     // Handle the response, maybe show a success or error message
   };
 
   return (
-    <div className="p-5">
-      <h1 className="text-2xl mb-4">Contact Me</h1>
-      <p className="mb-4">Feel free to reach out to me through the form below:</p>
-      
-      <form onSubmit={handleSubmit} className="space-y-4">
+    <motion.div
+      className="p-8 bg-gray-100 rounded-xl"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <motion.h1
+        className="text-3xl mb-6 font-bold text-center text-purple-600"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        Contact Me
+      </motion.h1>
 
-        <div>
-          <label className="block mb-1">Name:</label>
-          <input 
-            type="text" 
-            placeholder="Enter your name" 
-            className="w-full p-2 border rounded" 
-            value={formData.name} 
-            onChange={e => setFormData(prev => ({ ...prev, name: e.target.value }))} 
+      <motion.p
+        className="mb-8 text-center text-gray-600"
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+      >
+        Feel free to reach out to me through the form below:
+      </motion.p>
+
+      <motion.form
+        onSubmit={handleSubmit}
+        className="space-y-6"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7 }}
+      >
+        <div className="flex flex-col">
+          <label className="text-gray-700 font-semibold mb-2">Name:</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="p-3 border rounded shadow"
+            value={formData.name}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, name: e.target.value }))
+            }
           />
         </div>
 
-        <div>
-          <label className="block mb-1">Email:</label>
-          <input 
-            type="email" 
-            placeholder="Enter your email" 
-            className="w-full p-2 border rounded" 
-            value={formData.email} 
-            onChange={e => setFormData(prev => ({ ...prev, email: e.target.value }))} 
+        <div className="flex flex-col">
+          <label className="text-gray-700 font-semibold mb-2">Email:</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="p-3 border rounded shadow"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, email: e.target.value }))
+            }
           />
         </div>
 
-        
-        <div>
-          <label className="block mb-1">Phone:</label>
-          <input 
-            type="tel" 
-            placeholder="Phone Number" 
-            className="w-full p-2 border rounded" 
-            value={formData.phone} 
-            onChange={e => setFormData(prev => ({ ...prev, phone: e.target.value }))} 
+        <div className="flex flex-col">
+          <label className="text-gray-700 font-semibold mb-2">Phone:</label>
+          <input
+            type="tel"
+            placeholder="Phone Number"
+            className="p-3 border rounded shadow"
+            value={formData.phone}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, phone: e.target.value }))
+            }
           />
         </div>
 
-        <div>
-          <label className="block mb-1">Subject:</label>
-          <input 
-            type="text" 
-            placeholder="Subject" 
-            className="w-full p-2 border rounded" 
-            value={formData.subject} 
-            onChange={e => setFormData(prev => ({ ...prev, subject: e.target.value }))} 
+        <div className="flex flex-col">
+          <label className="text-gray-700 font-semibold mb-2">Subject:</label>
+          <input
+            type="text"
+            placeholder="Subject"
+            className="p-3 border rounded shadow"
+            value={formData.subject}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, subject: e.target.value }))
+            }
           />
         </div>
 
-
-        <div>
-          <label className="block mb-1">Message:</label>
-          <textarea 
-            placeholder="Enter your message" 
-            rows={4} 
-            className="w-full p-2 border rounded" 
-            value={formData.message} 
-            onChange={e => setFormData(prev => ({ ...prev, message: e.target.value }))} 
+        <div className="flex flex-col">
+          <label className="text-gray-700 font-semibold mb-2">Message:</label>
+          <textarea
+            placeholder="Enter your message"
+            rows={4}
+            className="p-3 border rounded shadow resize-none"
+            value={formData.message}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, message: e.target.value }))
+            }
           ></textarea>
         </div>
 
-        <button 
-          type="submit" 
-          className="py-2 px-4 bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none focus:bg-green-700"
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileFocus={{ boxShadow: "0 0 8px 2px rgba(148, 0, 211, 0.3)" }}
+          type="submit"
+          className="w-full py-3 px-6 mt-8 mb-6 text-white font-semibold rounded transition duration-500 ease-in-out bg-gradient-to-r from-purple-600 via-pink-500 to-yellow-500"
         >
           Submit
-        </button>
-      </form>
+        </motion.button>
+      </motion.form>
+
       <Footer />
-    </div>
+    </motion.div>
   );
-}
+};
 
 export default ContactPage;
