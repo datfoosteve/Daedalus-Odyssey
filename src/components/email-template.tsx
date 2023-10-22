d3a9c1d6ee255acc94339ba5f41ffce343a453cb
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2023-10-22 06:38:43 -0500

    used public image

--- src/components/email-template.tsx
+++ src/components/email-template.tsx
@@ -12,7 +12,7 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName, email, phone, subject, message
 }) => (
   <div className="bg-gray-100 p-8 rounded-lg max-w-lg mx-auto my-8">
-  <img src="https://stephenputhenpurackal.com/public/images/icon.png" alt="Website Logo" className="block mx-auto mb-6" />
+  <img src="https://i.imgur.com/2gOrsMv.png" alt="Website Logo" style={{ display: 'block', margin: '0 auto', marginBottom: '20px' }} />
   <h1 className="text-xl font-semibold mb-4 text-gray-800">Thank You, {firstName}!</h1>
   <p className="text-gray-700 mb-6">
     I&apos;ve received your message and appreciate you reaching out. I&apos;ll make sure to review your inquiry and get back to you as soon as possible. Looking forward to discussing how I can assist you with your web development needs!
